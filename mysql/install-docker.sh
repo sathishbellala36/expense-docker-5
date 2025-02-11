486cf5d44c9e54ff04eb7b064df8a6de24397f57
sathishbellala36 <sathishbellala36@gmail.com>
2025-02-11 19:10:48 +0530

    docker

---
+++ mysql/install-docker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+yum install -y yum-utils
+yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+systemctl start docker
+usermod -aG docker ec2-user
